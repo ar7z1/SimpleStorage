b60e3049e08f94c9be8b53f00556a450081c281e
art <art@skbkontur.ru>
2014-11-29 01:56:41 +0500

    Сделал проект с тестами и написал первый тест.

---
+++ Service.Tests/Controllers/ServiceFunctionalTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Http;
+using NUnit.Framework;
+
+namespace Service.Tests.Controllers
+{
+    public class ServiceFunctionalTests : TestBase
+    {
+        private const string endpoint = "http://localhost:15000/";
+
+        [Test]
+        public void Get_Always_ShouldReturnOk()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync(endpoint + "api/values").Result;
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+        }
+    }
+}
---
+++ Service.Tests/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoRhinoMock;
+
+namespace Service.Tests
+{
+    [TestFixture]
+    public abstract class TestBase
+    {
+        [SetUp]
+        public virtual void SetUp()
+        {
+            fixture = new Fixture();
+            fixture.Customize(new AutoRhinoMockCustomization());
+        }
+
+        [TearDown]
+        public virtual void TearDown()
+        {
+        }
+
+        protected Fixture fixture;
+    }
+}
