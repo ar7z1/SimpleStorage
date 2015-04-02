a6219beb45ba983874588cb15733ec9ea21645c1
kokovin <kokovin@kokovin.kontur>
2015-04-02 20:03:52 +0500

    Сделали интерфейс для клиента

--- SimpleStorage/Client/ISimpleStorageClient.cs
+++ SimpleStorage/Client/ISimpleStorageClient.cs
@@ -1,7 +1,13 @@
-﻿namespace Client
+﻿using System.Collections.Generic;
+using Domain;
+
+namespace Client
 {
     public interface ISimpleStorageClient
     {
-
+        void Put(string id, Value value);
+        IEnumerable<ValueWithId> GetAll();
+        Value Get(string id);
+        void Delete(string id);
     }
 }
