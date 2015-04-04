1a894342ee9841f504ca0224813282a481b62a46
art <art@skbkontur.ru>
2015-04-04 18:57:11 +0500

    Удалил ненужные namespace-ы

--- SimpleStorage/SimpleStorage.Tests/ReplicationAndConsistency/Task1Tests.cs
+++ SimpleStorage/SimpleStorage.Tests/ReplicationAndConsistency/Task1Tests.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
-using System.Web.Http;
 using Client;
 using Domain;
-using Microsoft.Owin.Hosting;
 using NUnit.Framework;
 using NUnit.Framework.Constraints;
 
