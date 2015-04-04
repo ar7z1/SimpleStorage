5aef035a3290a0b3ddbe59324cdd53f98f267058
art <art@skbkontur.ru>
2015-04-04 13:46:42 +0500

    Убрал использование RhinoMocks  и AutoFixture.

--- SimpleStorage/SimpleStorage.Tests/Controllers/ValuesControllerFunctionalTests.cs
+++ SimpleStorage/SimpleStorage.Tests/Controllers/ValuesControllerFunctionalTests.cs
@@ -1,12 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Net;
 using System.Net.Http;
 using Client;
 using Domain;
 using Microsoft.Owin.Hosting;
 using NUnit.Framework;
-using Ploeh.AutoFixture;
 using SimpleStorage.Infrastructure;
 using SimpleStorage.IoC;
 
@@ -15,33 +13,32 @@ namespace SimpleStorage.Tests.Controllers
     [TestFixture]
     public class ValuesControllerFunctionalTests
     {
+        private const int port = 15000;
+        private readonly string endpoint = string.Format("http://127.0.0.1:{0}/", port);
+        private SimpleStorageClient client;
+
         [SetUp]
         public void SetUp()
         {
-            fixture = new Fixture();
             client = new SimpleStorageClient(endpoint);
             var container = IoCFactory.GetContainer();
             container.Configure(c => c.For<IStateRepository>().Use(new StateRepository()));
             var operationLog = new OperationLog();
             container.Configure(c => c.For<IOperationLog>().Use(operationLog));
-            container.Configure(c => c.For<IStorage>().Use(new Storage(operationLog, container.GetInstance<ValueComparer>())));
+            container.Configure(
+                c => c.For<IStorage>().Use(new Storage(operationLog, container.GetInstance<ValueComparer>())));
         }
-
-        private const int port = 15000;
-        private readonly string endpoint = string.Format("http://127.0.0.1:{0}/", port);
-        private Fixture fixture;
-        private SimpleStorageClient client;
 
         [Test]
         public void GetAll_NonEmptyStorage_ShouldReturnAll()
         {
-            var id = fixture.Create<string>();
-            var value = fixture.Create<Value>();
+            const string id = "id";
+            var value = new Value {Content = "content"};
             using (WebApp.Start<Startup>(string.Format("http://+:{0}/", port)))
             {
                 client.Put(id, value);
 
-                IEnumerable<ValueWithId> actual = client.GetAll().ToArray();
+                var actual = client.GetAll().ToArray();
 
                 Assert.That(actual.ToArray(),
                     Has.Some.Matches<ValueWithId>(v => v.Id == id && v.Value.Content == value.Content));
@@ -51,13 +48,13 @@ namespace SimpleStorage.Tests.Controllers
         [Test]
         public void Get_KnownId_ShouldReturnValue()
         {
-            var id = fixture.Create<string>();
-            var value = fixture.Create<Value>();
+            const string id = "id";
+            var value = new Value {Content = "content"};
 
             using (WebApp.Start<Startup>(string.Format("http://+:{0}/", port)))
             {
                 client.Put(id, value);
-                Value actual = client.Get(id);
+                var actual = client.Get(id);
                 Assert.That(actual.Content, Is.EqualTo(value.Content));
             }
         }
@@ -70,11 +67,11 @@ namespace SimpleStorage.Tests.Controllers
                 using (var httpClient = new HttpClient())
                 {
                     using (
-                        HttpResponseMessage response =
+                        var response =
                             httpClient.PostAsync(endpoint + "api/admin/stop", new ByteArrayContent(new byte[0])).Result)
                         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
 
-                    using (HttpResponseMessage response = httpClient.GetAsync(endpoint + "api/values").Result)
+                    using (var response = httpClient.GetAsync(endpoint + "api/values").Result)
                         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.InternalServerError));
                 }
             }
@@ -87,13 +84,18 @@ namespace SimpleStorage.Tests.Controllers
             {
                 using (var httpClient = new HttpClient())
                 {
-                    using (HttpResponseMessage response = httpClient.PostAsync(endpoint + "api/admin/stop", new ByteArrayContent(new byte[0])).Result)
+                    using (
+                        var response =
+                            httpClient.PostAsync(endpoint + "api/admin/stop", new ByteArrayContent(new byte[0])).Result)
                         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
 
-                    using (HttpResponseMessage response = httpClient.PostAsync(endpoint + "api/admin/start", new ByteArrayContent(new byte[0])).Result)
+                    using (
+                        var response =
+                            httpClient.PostAsync(endpoint + "api/admin/start", new ByteArrayContent(new byte[0])).Result
+                        )
                         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NoContent));
 
-                    using (HttpResponseMessage response = httpClient.GetAsync(endpoint + "api/values").Result)
+                    using (var response = httpClient.GetAsync(endpoint + "api/values").Result)
                         Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
                 }
             }
@@ -102,11 +104,11 @@ namespace SimpleStorage.Tests.Controllers
         [Test]
         public void Get_UnknownId_ShouldReturnNotFound()
         {
-            string requestUri = endpoint + "api/values/" + fixture.Create<string>();
+            var requestUri = endpoint + "api/values/unknownId";
 
             using (WebApp.Start<Startup>(string.Format("http://+:{0}/", port)))
-            using (var client = new HttpClient())
-            using (HttpResponseMessage response = client.GetAsync(requestUri).Result)
+            using (var httpClient = new HttpClient())
+            using (var response = httpClient.GetAsync(requestUri).Result)
                 Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
         }
     }
