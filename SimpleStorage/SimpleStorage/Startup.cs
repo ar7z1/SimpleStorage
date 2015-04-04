edff5ff85fdfaa0a9c38cc13bd81d9a4328c493e
kokovin <kokovin@kokovin.kontur>
2015-04-04 23:41:58 +0500

    internal actions and internal client

--- SimpleStorage/SimpleStorage/Startup.cs
+++ SimpleStorage/SimpleStorage/Startup.cs
@@ -11,7 +11,7 @@ namespace SimpleStorage
         public void Configuration(IAppBuilder appBuilder)
         {
             var config = new HttpConfiguration {DependencyResolver = new DependencyResolver(IoCFactory.GetContainer())};
-            config.Routes.MapHttpRoute("AdminApi", "api/admin/{action}", new {controller = "Admin"});
+            config.Routes.MapHttpRoute("AdminApi", "api/admin/{action}/{id}", new { controller = "Admin", id = RouteParameter.Optional });
             config.Routes.MapHttpRoute("OperationLogApi", "api/operations", new {controller = "Operations", action = "Get"});
             config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}", new {id = RouteParameter.Optional});
             config.Services.Replace(typeof (IExceptionLogger), new ConsoleExceptionLogger());
