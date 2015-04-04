9be87fce468190babd73c6a8daf165ce48953d87
kokovin <kokovin@kokovin.kontur>
2015-04-04 20:18:57 +0500

    Добавил тесты на Ring

--- SimpleStorage/SimpleStorage.Tests/ReplicationAndConsistency/Task1Tests.cs
+++ SimpleStorage/SimpleStorage.Tests/ReplicationAndConsistency/Task1Tests.cs
@@ -43,7 +43,7 @@ namespace SimpleStorage.Tests.ReplicationAndConsistency
             {
                 ActionOnReplica(replica, httpClient, "stop");
 
-                for (int i = 0; i < 2; ++i)
+                for (int i = 0; i < 10; ++i)
                 {
                     fullTopologyClient.Get(id);
                 }
