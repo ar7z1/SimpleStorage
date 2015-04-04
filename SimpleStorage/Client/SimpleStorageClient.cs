cd89c15d5b20b5b06780b5d0f0c478b61647f004
kokovin <kokovin@kokovin.kontur>
2015-04-04 20:01:30 +0500

    клиент для master-slave

--- SimpleStorage/Client/SimpleStorageClient.cs
+++ SimpleStorage/Client/SimpleStorageClient.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using Domain;
@@ -8,13 +7,13 @@ namespace Client
 {
     public class SimpleStorageClient : ISimpleStorageClient
     {
-        private readonly IEnumerable<string> endpoints;
+        private readonly string[] endpoints;
 
         public SimpleStorageClient(params string[] endpoints)
         {
             if (endpoints == null || !endpoints.Any())
                 throw new ArgumentException("Empty endpoints!", "endpoints");
-            this.endpoints = endpoints;
+            this.endpoints = endpoints.ToArray();
         }
 
         public void Put(string id, Value value)
@@ -27,13 +26,21 @@ namespace Client
 
         public Value Get(string id)
         {
-            var requestUri = endpoints.First() + "api/values/" + id;
-            using (var client = new HttpClient())
-            using (var response = client.GetAsync(requestUri).Result)
+            var random = new Random();
+            for (int i = 0; i < 5; i++)
             {
-                response.EnsureSuccessStatusCode();
-                return response.Content.ReadAsAsync<Value>().Result;
+                try
+                {
+                    var requestUri = endpoints[random.Next(endpoints.Count() - 1)] + "api/values/" + id;
+                    using (var client = new HttpClient())
+                    using (var response = client.GetAsync(requestUri).Result)
+                    {
+                        response.EnsureSuccessStatusCode();
+                        return response.Content.ReadAsAsync<Value>().Result;
+                    }
+                } catch {}
             }
+            throw new HttpRequestException("All attempts failed");
         }
     }
 }
