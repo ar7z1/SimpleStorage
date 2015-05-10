0db802b9013119747b21739db7eaf476c3a9af00
art <zinenkoartem@gmail.com>
2015-05-11 00:58:44 +0500

    Немного порефакторил тесты на шардинг 

--- SimpleStorage/SimpleStorage.Tests/ShardingTests.cs
+++ SimpleStorage/SimpleStorage.Tests/ShardingTests.cs
@@ -5,96 +5,100 @@ using System.Net.Http;
 using Client;
 using Domain;
 using NUnit.Framework;
+using SimpleStorage.Configuration;
+using Configuration;
+using System.Net;
 
 namespace SimpleStorage.Tests
 {
-    [TestFixture]
-    [Ignore]
-    public class ShardingTests
-    {
-        private const int port1 = 15000;
-        private const int port2 = 15001;
-        private const int port3 = 15002;
-        private readonly string[] endpoints = {endpoint1, endpoint2, endpoint3};
-        private SimpleStorageClient simpleStorageClient;
+	[TestFixture]
+	[Ignore]
+	public class ShardingTests
+	{
+		private const int port1 = 15000;
+		private const int port2 = 15001;
+		private const int port3 = 15002;
+		private readonly string[] endpoints = { endpoint1, endpoint2, endpoint3 };
+		private SimpleStorageClient simpleStorageClient;
+		private readonly IShardsConfiguration shardsConfiguration = new StubShardsConfiguration() { 
+			Shards = new[] {
+				new IPEndPoint(IPAddress.Loopback, port1),
+				new IPEndPoint(IPAddress.Loopback, port2),
+				new IPEndPoint(IPAddress.Loopback, port3)
+			}
+		};
 
-        [SetUp]
-        public void SetUp()
-        {
-            simpleStorageClient = new SimpleStorageClient(endpoints);
-        }
+		[SetUp]
+		public void SetUp()
+		{
+			simpleStorageClient = new SimpleStorageClient(endpoints);
+		}
 
-        [Test]
-        public void Sharding_EachShard_ShouldNotContainAllData()
-        {
-            using (SimpleStorageTestHelpers.StartService(port1))
-            using (SimpleStorageTestHelpers.StartService(port2))
-            using (SimpleStorageTestHelpers.StartService(port3))
-            {
-                for (var i = 0; i < 100; i++)
-                    simpleStorageClient.Put(Guid.NewGuid().ToString(), new Value {Content = "content"});
+		[Test]
+		public void Sharding_EachShard_ShouldNotContainAllData()
+		{
+			using (SimpleStorageTestHelpers.StartService(port1, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port2, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port3, shardsConfiguration)) {
+				for (var i = 0; i < 100; i++)
+					simpleStorageClient.Put(Guid.NewGuid().ToString(), new Value { Content = "content" });
 
-                Assert.That(GetAll(endpoint1).ToArray(), Has.Length.LessThan(100));
-                Assert.That(GetAll(endpoint2).ToArray(), Has.Length.LessThan(100));
-                Assert.That(GetAll(endpoint3).ToArray(), Has.Length.LessThan(100));
-            }
-        }
+				Assert.That(GetAll(endpoint1).ToArray(), Has.Length.LessThan(100));
+				Assert.That(GetAll(endpoint2).ToArray(), Has.Length.LessThan(100));
+				Assert.That(GetAll(endpoint3).ToArray(), Has.Length.LessThan(100));
+			}
+		}
 
-        [Test]
-        public void Sharding_AllShards_ShouldContainSomeData()
-        {
-            using (SimpleStorageTestHelpers.StartService(port1))
-            using (SimpleStorageTestHelpers.StartService(port2))
-            using (SimpleStorageTestHelpers.StartService(port3))
-            {
-                for (var i = 0; i < 100; i++)
-                    simpleStorageClient.Put(Guid.NewGuid().ToString(), new Value {Content = "content"});
+		[Test]
+		public void Sharding_AllShards_ShouldContainSomeData()
+		{
+			using (SimpleStorageTestHelpers.StartService(port1, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port2, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port3, shardsConfiguration)) {
+				for (var i = 0; i < 100; i++)
+					simpleStorageClient.Put(Guid.NewGuid().ToString(), new Value { Content = "content" });
 
-                Assert.That(GetAll(endpoint1).ToArray(), Has.Length.GreaterThan(0));
-                Assert.That(GetAll(endpoint2).ToArray(), Has.Length.GreaterThan(0));
-                Assert.That(GetAll(endpoint3).ToArray(), Has.Length.GreaterThan(0));
-            }
-        }
+				Assert.That(GetAll(endpoint1).ToArray(), Has.Length.GreaterThan(0));
+				Assert.That(GetAll(endpoint2).ToArray(), Has.Length.GreaterThan(0));
+				Assert.That(GetAll(endpoint3).ToArray(), Has.Length.GreaterThan(0));
+			}
+		}
 
-        [Test]
-        public void Sharding_Always_ShouldSaveAllData()
-        {
-            using (SimpleStorageTestHelpers.StartService(port1))
-            using (SimpleStorageTestHelpers.StartService(port2))
-            using (SimpleStorageTestHelpers.StartService(port3))
-            {
-                var items = new List<KeyValuePair<string, Value>>();
-                for (var i = 0; i < 100; i++)
-                {
-                    var id = Guid.NewGuid().ToString();
-                    var value = new Value {Content = "content"};
-                    items.Add(new KeyValuePair<string, Value>(id, value));
-                    simpleStorageClient.Put(id, value);
-                }
+		[Test]
+		public void Sharding_Always_ShouldSaveAllData()
+		{
+			using (SimpleStorageTestHelpers.StartService(port1, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port2, shardsConfiguration))
+			using (SimpleStorageTestHelpers.StartService(port3, shardsConfiguration)) {
+				var items = new List<KeyValuePair<string, Value>>();
+				for (var i = 0; i < 100; i++) {
+					var id = Guid.NewGuid().ToString();
+					var value = new Value { Content = "content" };
+					items.Add(new KeyValuePair<string, Value>(id, value));
+					simpleStorageClient.Put(id, value);
+				}
 
-                foreach (var item in items)
-                {
-                    var actual = simpleStorageClient.Get(item.Key);
-                    Assert.That(actual.Content, Is.EqualTo(item.Value.Content));
-                    Assert.That(actual.IsDeleted, Is.EqualTo(item.Value.IsDeleted));
-                    Assert.That(actual.Revision, Is.EqualTo(item.Value.Revision));
-                }
-            }
-        }
+				foreach (var item in items) {
+					var actual = simpleStorageClient.Get(item.Key);
+					Assert.That(actual.Content, Is.EqualTo(item.Value.Content));
+					Assert.That(actual.IsDeleted, Is.EqualTo(item.Value.IsDeleted));
+					Assert.That(actual.Revision, Is.EqualTo(item.Value.Revision));
+				}
+			}
+		}
 
-        private IEnumerable<ValueWithId> GetAll(string endpoint)
-        {
-            var requestUri = endpoint + "api/admin/getAllLocalData";
-            using (var httpClient = new HttpClient())
-            using (var response = httpClient.GetAsync(requestUri).Result)
-            {
-                response.EnsureSuccessStatusCode();
-                return response.Content.ReadAsAsync<IEnumerable<ValueWithId>>().Result;
-            }
-        }
+		private IEnumerable<ValueWithId> GetAll(string endpoint)
+		{
+			var requestUri = endpoint + "api/admin/getAllLocalData";
+			using (var httpClient = new HttpClient())
+			using (var response = httpClient.GetAsync(requestUri).Result) {
+				response.EnsureSuccessStatusCode();
+				return response.Content.ReadAsAsync<IEnumerable<ValueWithId>>().Result;
+			}
+		}
 
-        private static readonly string endpoint1 = string.Format("http://127.0.0.1:{0}/", port1);
-        private static readonly string endpoint2 = string.Format("http://127.0.0.1:{0}/", port2);
-        private static readonly string endpoint3 = string.Format("http://127.0.0.1:{0}/", port3);
-    }
+		private static readonly string endpoint1 = string.Format("http://127.0.0.1:{0}/", port1);
+		private static readonly string endpoint2 = string.Format("http://127.0.0.1:{0}/", port2);
+		private static readonly string endpoint3 = string.Format("http://127.0.0.1:{0}/", port3);
+	}
 }
