30a6a37fbebdb44d83646c7f0b7ca859b0d352f6
art <zinenkoartem@gmail.com>
2016-03-10 19:37:12 +0500

    Выпилил Core.Web

---
+++ SimpleStorage/SimpleStorage/ConsoleExceptionLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Http.ExceptionHandling;
+
+namespace Core.Web
+{
+    public class ConsoleExceptionLogger : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            Console.Error.WriteLine(context.Exception);
+        }
+    }
+}
